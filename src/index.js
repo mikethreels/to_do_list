8f9323145b2a1a06bc1272dcd70d8b66efc60499
Michael Threels <michaelthreels@gmail.com>
2020-09-28 21:49:47 +0200

    chore: deleted commented code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import inputForms from './landing';
-// import nav from './nav';
 import aside from './aside';
 import project from './project';
 
-// nav();
 project();
 inputForms.landing();
 inputForms.toDoForm();
--- src/landing.js
+++ src/landing.js
@@ -1,18 +1,8 @@
 /* eslint-disable no-restricted-globals */
-/* eslint-disable import/no-cycle */
 import StorageLogic from './form_submission';
 import project from './project';
 
 const inputForms = (() => {
-  // const visibility = () => {
-  //   const form = document.getElementById('content');
-  //   if (form.attributes.class.value === 'landing') {
-  //     form.attributes.class.value = 'landing hidden';
-  //   } else {
-  //     form.attributes.class.value = 'landing';
-  //   }
-  // };
-
   const toDo = () => {
     const form = document.getElementById('to_do_container');
     if (form.attributes.class.value === 'to_do_container') {
@@ -60,8 +50,7 @@ const inputForms = (() => {
       const head = localStorage.key(i);
       link.append(head);
       link.classList.add('aside_link');
-      link.addEventListener('click', (e) => {
-        e.preventDefault();
+      link.addEventListener('click', () => {
         project(localStorage.key(i));
       });
       li.append(link);
